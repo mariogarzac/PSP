1610a8a6dd7d8dea37b3edbe13f8792b38653053
Mario Garza <mariogarzac99@gmail.com>
2022-03-28 21:39:30 -0600

    Added some comments to main.py and made a Calculador variable

--- Programa3/main.py
+++ Programa3/main.py
@@ -1,44 +1,31 @@
 #!/usr/bin/python
-# Este programa tiene como fín leer archivos y contar cuantas lineas
-# de código tiene y contar cuantas son de base, item, modificadas o agregadas
-# 
+# Este programa tiene como fín leer archivos y calcular 
+# numero de pares, xk, r, r^2, b0, b1 y yk
 #
 # Mario Garza Chapa A01720245
 #
 # Creado: 23/02/2022
 
 
-import queue
-from Lector import Lector
+from Calculador import Calculador
 from Verificador import Verificador
 
-
+#.d=8
+#.m=6
 # MAIN DEL PROGRAMA
 def main():
 
     ver = Verificador()
-    lec = Lector()
+    calc = Calculador()
     
-    #Queue para ir almacenado los valores de cada archivo
-    order = queue.Queue() 
-    
-    print("-------------------------------------------")
-    print("-Para terminar la ejecución escribir exit -")
-    print("-------------------------------------------")
-    fileName = input("Nombre del archivo para leer: ")
-    while(fileName != "exit"):
+    #Se lee el nombre del archivo
+    fileName = input("Nombre del archvio a leer: ")
 
-        # Verifica que el archivo esté en el mismo directorio
-        # Si está, lo leerá y contará las líneas
-        if(fileName != "" and ver.verifica(fileName)):            
-            order.put(lec.procesarArchivo(fileName))
-        fileName = input("Nombre del archivo para leer: ")
-                
-    #Ordenará e imprimirá los valores
-    if (fileName == "exit"):
-        lec.calculateClass(order)
-    input("Presiona ENTER para cerrar: ")
-
+    # Verifica que el archivo esté en el PATH
+    # Si está, lo leerá y realizará los calculos
+    if(ver.verifica(fileName)):            
+        calc.getElements(fileName)        
+            
 
 if __name__ == "__main__":
     main()
