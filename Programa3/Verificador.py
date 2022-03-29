049fb4d750900342851fa43767b07e933c35d38d
Mario Garza <mariogarzac99@gmail.com>
2022-03-28 21:35:21 -0600

    Closed file after checking if its empty

--- Programa3/Verificador.py
+++ Programa3/Verificador.py
@@ -6,12 +6,12 @@
 # Creado: 16/03/2022
 
 import os
-from os.path import exists
 
 class Verificador:
     # Verifica si el archivo existe o si el archivo está vacío
     # En caso de que no esté vacío, regresará True para que se ejecute
     # El lector
+    #.i
     def verifica(self, fileName):
         # if para revisar que exista y no esté vacío
         try:
@@ -20,12 +20,13 @@ class Verificador:
         except FileNotFoundError:
             print("No existe el archivo en el PATH actual, revisa el nombre.")
             print("Path actual: {}".format(os.getcwd()))
-            return False    
-        #Si el archivo está vacio     
-        
+            return False 
+
+        #Si el archivo está vacio 
         if(os.path.getsize(fileName) == 0):
-               print("Archivo vacio. ")
-               fp.close()
-               return False
-           
+            print("Archivo vacio. ")
+            fp.close()
+            return False            
+        
+        fp.close()
         return True
