cf681617633f298f762ebdf83984a0d8c7825339
Mario Garza <mariogarzac99@gmail.com>
2022-03-30 21:03:59 -0600

    Fixed code crashing with certain file types

--- Programa3/Calculador.py
+++ Programa3/Calculador.py
@@ -8,7 +8,9 @@
 #
 # Creado: 28/03/2022
 
+
 import math
+
 class Calculador:
     # Función para recibir los elementos calculados
     #.i
@@ -19,16 +21,21 @@ class Calculador:
         y = []
 
         with open(fileName, "r") as arch:
-            xk = arch.readline()
-            for line in arch:
-                line = (line.strip()).split(',')                
-                x.append(float(line[0]))
-                y.append(float(line[1]))
-            
-            if (len(x) != len(y)):
-                print("Hay elementos que no tienen par. ")
-                arch.close()
-                return 0                 
+            xk = arch.readline()  
+            try:          
+                for line in arch:
+                    line = (line.strip()).split(',')                
+                    x.append(float(line[0]))
+                    y.append(float(line[1]))
+                
+            except ValueError:
+                print("ERROR. El archivo es incompatible con este programa. ")
+                return 0            
+            except IndexError:
+                print("ERROR. Hay un valor sin par. ")
+                return 0
+            finally:
+                arch.close()                
 
             values = self.getValues(x,y, int(xk))
             self.prettyPrint(values)
@@ -76,7 +83,7 @@ class Calculador:
     def getB1(self, n, xAvg, yAvg, sumXY, xSquared):
         top = sumXY - (n * xAvg * yAvg)
         bottom = xSquared - (n * pow(xAvg,2))
-        return (top/bottom)
+        return top/bottom 
 
     # Calcula yk
     #.i
@@ -86,13 +93,14 @@ class Calculador:
     # Imprime los elementos calculados 
     #.i      
     def prettyPrint(self, values):
+        #orden en el arreglo values
         #[n, r, rSquared, b0, b1, yk, xk]
-        print("N = {}".format(round(values[0]),5))
-        print("xk = {}".format(round(values[6]),5))
-        print("r = {}".format(round(values[1]),5))
-        print("rSquared = {}".format(round(values[2]),5))
-        print("b0 = {}".format(round(values[3]),5))
-        print("b1 = {}".format(round(values[4]),5))
-        print("yk = {}".format(round(values[5]),5))
+        print(f"N = {values[0]}")
+        print(f"xk = {values[6]}")
+        print(f"r = {values[1]}")
+        print(f"rSquared = {values[2]}")
+        print(f"b0 = {values[3]}")
+        print(f"b1 = {values[4]}")
+        print(f"yk = {values[5]}")
 
 
